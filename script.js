565b8defffd4e92755451b439f59a841f4541179
murunaerlisrivas <erlis.rivas@muruna.cl>
2024-12-30 12:16:58 -0300

    cambios en script

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const rows = document.querySelectorAll("tbody tr.feature");
 
     rows.forEach(row => {
         const despCell = row.querySelector("td.desp.border");
-        if (!despCell || !despCell.textContent.trim()) {
+        if (!despCell || (!despCell.textContent.trim() && !despCell.querySelector("div"))) {
             row.style.display = "none"; // Oculta la fila si la celda está vacía
         }
     });
